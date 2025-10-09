8e1c4943614cbff269f4103ad4db5e4f41197934
DrFawaz-Alhassani <fawazalhassani2001@gmail.com>
2025-10-09 20:32:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -18,12 +18,12 @@ if (wr.innerText=="Dark mode") {
 
 };
 
-
 let las= document.getElementById("s")
 let lass = document.getElementById("ss")
 let lan = document.getElementById("lan");
 let btn = document.getElementById("btn");
 let h2= document.getElementById("h2");
+
 
 function lange () {
   if (lan.innerText=="عربي") {
@@ -32,15 +32,17 @@ function lange () {
     las.innerText="اسم المستخدم";
     lass.innerText="كلمة المرور";
     h2.innerText="تسجيل الدخول";
+  document.getElementById('fo').innerHTML = 'هل نسيت كلمة المرور؟ <a id="repa" href="#">استرجاع كلمة المرور</a>';
+  document.getElementById('do').innerHTML = 'هل لاتمتلك حساب لدينا ..؟<a id="forget" href="#"> انشاء حسابك الآن </a>';
     
-
-  } else if(lan.innerText=="English"){
+      } else if(lan.innerText=="English"){
     lan.innerText="عربي";
     btn.innerText="Log in";
     las.innerText="Username";
     lass.innerText="Password";
     h2.innerText="Log in Form";
+    document.getElementById('fo').innerHTML = 'Do not forget password <a id="repa" href="#"> : forget password now </a>';
+ document.getElementById('do').innerHTML = 'Do not have account <a id="forget" href="#"> Create a account now !!! </a>';
     
-  
   }
 };
