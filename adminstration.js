c01ff8ab7d87af81ec8936bf70c46c573e8239d4
DrFawaz-Alhassani <fawazalhassani2001@gmail.com>
2025-10-23 23:09:02 +0300

    Add files via upload

--- adminstration.js
+++ adminstration.js
@@ -38,9 +38,63 @@ function hedilist() {
   }
 }
 
+function fename(param) {
+    const aa = localStorage.getItem("Username");
+    const ab = localStorage.getItem("Email");
+    const ac = localStorage.getItem("password");
+    const ad = localStorage.getItem("repassword");
+    const tb = document.getElementById("tbb1").getElementsByTagName('tbody')[0];
+    const ce = tb.insertRow();
+    
+    
+   
+   
+   if(aa && ab && ac){
+     const countrows = tb.rows.length - 1; // احصل على عدد الصفوف
+    ce.insertCell(0).innerText = countrows -1; // الرقم التسلسلي
+    
+     const bea = document.getElementById('sph4');
+   bea.innerText = countrows;
+   
+    ce.insertCell(1).innerText = aa; // الاسم
+    ce.insertCell(2).innerText = ac; // كلمة المرور
+    ce.insertCell(3).innerText = ab; // البريد الإلكتروني
+
+    // أضف الأزرار إلى الخلايا باستخدام innerHTML
+    ce.insertCell(4).innerHTML = '<button type="button" onclick="editRow(this)">تعديل</button>';
+    ce.insertCell(5).innerHTML = '<button type="button" onclick="deleteRow(this)">حذف</button>';
+    
+    localStorage.removeItem("Username");
+    localStorage.removeItem("Email");
+    localStorage.removeItem("password");
+    localStorage.removeItem("repassword");
+   }
+   else{
+   }
+   
+}
 
 
-//كود للتعامل مع حذف عنصر من قاعدة بياناتك  بناءا على رقم المعرف 
-function deleteforid(){
-  
-};
+window.onload = fename();
+
+// دالة لتعديل صف
+function editRow(button) {
+    
+    const row = button.parentNode.parentNode; 
+    const username = row.cells[1].innerText = window.prompt("Enter new value ! for uosername !");
+    const password = rows.classList[2].innerText= window.prompt("Enter new value ! for password!");
+    const email = row.cells[3].innerText= window.prompt("Enter new value ! for uosername !");
+    
+    
+}
+
+// دالة لحذف صف
+function deleteRow(button) {
+    const row = button.parentNode.parentNode; // الحصول على الصف
+    row.parentNode.removeChild(row); // حذف الصف من الجدول
+}
+
+
+
+// تنفيذ الدالة كل 20 ثانية
+setInterval(() => fename(), 5000);
--- main.js
+++ main.js
@@ -2,16 +2,33 @@ let bak= document.getElementById("sec");
 let box= document.getElementById("box");
 let wr = document.getElementById("dark")
 
+const boxx = document.getElementById('box');
+const forget = document.getElementById('forget');
+const email = document.getElementById("email11");
+const repassword = document.getElementById("repassword11");
+const lab1 = document.getElementById('email1');
+const lab2 = document.getElementById('repassword1');
+const inboxx = document.getElementById('inputboxx');
+const inboxx1 = document.getElementById('inputboxx1');
+const ve = document.getElementById('inputboxx12');
+//كود لارجاع رمز تحقق
+function renumber (){
+const renum = Math.floor(100000 + Math.random()* 900000);
+return renum.toString()
+};
+
+
+
 function gi(){
 if (wr.innerText=="Dark mode") {
   bak.style.background="#123";
-  bak.style.transition=".2s"
+  bak.style.transition="4s"
   box.style.border="2px solid rgba(28, 120, 59, 1)";
   wr.innerText="White Mode"
 } else if(wr.innerText=="White Mode") {
   bak.style.background="url('e.jpeg')";
   bak.style.backgroundSize='cover';
-  bak.style.transition=".2s";
+  bak.style.transition="4s";
   box.style.border="2px solid rgba(255, 255, 255, 0.5)";
   wr.innerText="Dark mode";
 }
@@ -28,22 +45,28 @@ let h2= document.getElementById("h2");
 
 function lange () {
   if (lan.innerText=="عربي") {
+    ve.innerText = "رمز التحقق";
     lan.innerText="English";
+    lab1.innerText="البريد الالكتروني";
+ lab2.innerText="اعد كتابة كلمة المرور";
     btn.innerText="تسجيل الدخول";
     las.innerText="اسم المستخدم";
     lass.innerText="كلمة المرور";
     h2.innerText="تسجيل الدخول";
   document.getElementById('fo').innerHTML = 'هل نسيت كلمة المرور؟ <a id="repa" href="#">استرجاع كلمة المرور</a>';
-  document.getElementById('do').innerHTML = 'هل لاتمتلك حساب لدينا ..؟<a id="forget" href="#"> انشاء حسابك الآن </a>';
+  document.getElementById('do').innerHTML = 'هل لاتمتلك حساب لدينا ..؟<a onclick="widthbocauto()" id="forget" href="#">انشاء حسابك الآن </a>';
     
       } else if(lan.innerText=="English"){
     lan.innerText="عربي";
     btn.innerText="Log in";
+    lab1.innerText="Email"
+    lab2.innerText="repassword"
     las.innerText="Username";
     lass.innerText="Password";
     h2.innerText="Log in Form";
+    ve.innerText = "Code";
     document.getElementById('fo').innerHTML = 'Do not forget password <a id="repa" href="#"> : forget password now </a>';
- document.getElementById('do').innerHTML = 'Do not have account <a id="forget" href="#"> Create a account now !!! </a>';
+ document.getElementById('do').innerHTML = 'Do not have account <a onclick="widthbocauto()" id="forget" href="#"> Create account</a>';
     
   }
 };
@@ -51,6 +74,9 @@ function lange () {
 
 let text1 = document.getElementById("emi");
 let pass = document.getElementById("password"); // تأكد من وجود حقل إدخال بهذا المعرف
+let text3 = document.getElementById("email11");
+let text4 = document.getElementById("repassword11");
+
 
 function validateCredentials() {
   // التحقق من اسم المستخدم وكلمة المرور
@@ -61,13 +87,75 @@ const user = localStorage.getItem("username");
 const pasr = localStorage.getItem("password");
 
 
-
-
   if (user=== "admin" && pasr==="1234") {
     window.open('home.html');
+    
   } else{
     
     text1.value = "خطأ في بياناتك عزيزي المستخدم"; 
     pass.value = "خطأ في كلمة المرور";
   }
 }
+
+
+// تغيير الوضع الى انشاء الحساب
+
+
+
+window.onload = function(){
+  email.style.display='none';
+  repassword.style.display= 'none';
+  lab1.style.display='none';
+  lab2.style.display='none';
+  ve.style.display = "none";
+  
+}
+
+function widthbocauto(){
+  const asaa = document.getElementById('forget');
+  if(asaa.innerText==='انشاء حسابك الآن' || asaa.innerText==='Create account'){
+    boxx.style.transition='3s';
+  wr.style.bottom='700px'
+  lan.style.bottom='700px'
+  boxx.style.height='650px';
+  email.style.display='block';
+  repassword.style.display= 'block';
+  lab1.style.display='block';
+  lab2.style.display='block';
+  inboxx.style.display='block'
+ inboxx1.style.display='block';
+ btn.innerText="Creat Account";
+ ve.style.display="block";
+ h2.innerText="Create account";
+  
+ btn.onclick=function (param) {
+   appenddatafortabel();
+ }
+ 
+  }else{
+    
+  }
+  
+}
+
+
+function appenddatafortabel(param) {
+  if (pass.value === text4.value && pass.value !=="" && text4.value!=="" && ve.value!=="") {
+    ve.innerText = renumber();
+    let cc = localStorage.setItem("Email",text3.value);
+let xx = localStorage.setItem("Username",text1.value);
+let nn = localStorage.setItem("password",pass.value);
+let bb = localStorage.setItem("repassword",text4.value);
+
+alert('Creat account successfully ')
+window.open('./administration.html')
+
+  } else {
+    alert('Error password ')
+  }
+
+}
+
+
+
+
